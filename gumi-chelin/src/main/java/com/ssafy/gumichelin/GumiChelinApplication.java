100890e6c1fa10f71209cb6df39eac5b9103f9ea
Joongwon Lee <wnddnjs823@ajou.ac.kr>
2023-11-14 01:38:37 +0900

    Upload Project files

---
+++ gumi-chelin/src/main/java/com/ssafy/gumichelin/GumiChelinApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.gumichelin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GumiChelinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GumiChelinApplication.class, args);
+	}
+
+}
---
+++ gumi-chelin/src/test/java/com/ssafy/gumichelin/GumiChelinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.gumichelin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GumiChelinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
